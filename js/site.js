693f82ad36eaa2603dc77f317433571c613a9965
josephStuff <93104277+josephStuff@users.noreply.github.com>
2022-01-18 14:07:03 -0500

    Hundo2  first save
    from template Apps Repo.

--- js/site.js
+++ js/site.js
@@ -1,6 +1,4 @@
 
-
-
 // display message to the user.
 
 function getMessage() {
@@ -11,4 +9,66 @@ function getMessage() {
         "success"
     );
     console.log(userMessage);
+}
+
+
+// Program Entry Point
+
+function getValues2() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    // check if they are in fact INTEGERS !
+
+    if(Number.isInteger(startValue) && Number.isInteger(endValue) ) {
+
+        // generate a list of NUMBERS
+        let numbers = generateNumbers(startValue, endValue);
+
+        // DISPLAY A LIST OF NUMBERS
+        displayNumbers(numbers);
+
+    } 
+    else {
+        Swal.fire({
+        icon: 'error',
+        title: 'Ooops...',
+        text: 'Only integers are allowed for Hundo'
+         })
+    }
+    
+}
+
+function generateNumbers(start, end) {
+
+    let numbers= [];
+    for (let i = start; i <= end; i++) {
+        numbers.push(i);
+    }
+
+    return numbers;
+
+}
+
+function displayNumbers(numbers) {
+
+    let templateRows = "";
+    for (let index = 0; index < numbers.length; index ++) {
+
+        if (index % 2 == 0) {
+            className = "even";
+        }
+        else {
+            className = "odd";
+        }
+
+        let row = `<tr><td class="${className}">${index}</td></tr>`;
+        templateRows += row;
+
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
 }
